7e76feb3594731d0ddb1875c907c47e2a9de131b
Stephen Hellicar <shellicar@gmail.com>
2025-08-18 15:40:17 +1000

    Remove now unused test helper.

--- test/expectInfoKeys.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'vitest';
-import type { WinstonInfo } from '../src/private/types';
-
-export const expectInfoKeys = (info: WinstonInfo, expectedKeys: string[]) => {
-  const actualKeys = Object.keys(info).sort();
-  const sortedExpectedKeys = [...expectedKeys].sort();
-  expect(actualKeys).toEqual(sortedExpectedKeys);
-};
